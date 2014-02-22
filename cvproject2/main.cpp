6e50b4c934266999d87ab9181b37b660abf0ebdd
noonuirbouda <yangxingjiang@gmail.com>
2014-02-22 12:04:38 -0800

    Initial commit from local

---
+++ cvproject2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cvproject2
+//
+//  Created by Xueqian Jiang on 2/22/14.
+//  Copyright (c) 2014 Xueqian Jiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
